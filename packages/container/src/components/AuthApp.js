6682c424a8e1ae298b3d2f1b3f249de771bc8274
Jayson <jayson@mac.local>
2021-04-10 15:34:31 +0800

    added auth subapp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,8 @@
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -11,15 +11,16 @@ export default () => {
       initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
-        
-        if(pathname !== nextPathname){
+
+        if (pathname !== nextPathname) {
           history.push(nextPathname);
         }
       },
+      onSignIn,
     });
 
-    history.listen(onParentNavigate)
+    history.listen(onParentNavigate);
   }, []);
-  
+
   return <div ref={ref} />;
 };
